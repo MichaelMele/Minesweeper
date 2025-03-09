b5e635f993e2c389d33557575ca86142cc8b06a7
michael <michaemele365@gmail.com>
2025-03-08 16:46:37 -0800

    added thumbnail

--- Project_1/My Project/Resources.Designer.vb
+++ Project_1/My Project/Resources.Designer.vb
@@ -39,7 +39,7 @@ Namespace My.Resources
         Friend ReadOnly Property ResourceManager() As Global.System.Resources.ResourceManager
             Get
                 If Object.ReferenceEquals(resourceMan, Nothing) Then
-                    Dim temp As Global.System.Resources.ResourceManager = New Global.System.Resources.ResourceManager("Project_1.Resources", GetType(Resources).Assembly)
+                    Dim temp As Global.System.Resources.ResourceManager = New Global.System.Resources.ResourceManager("Minesweeper.Resources", GetType(Resources).Assembly)
                     resourceMan = temp
                 End If
                 Return resourceMan
@@ -76,6 +76,16 @@ Namespace My.Resources
         Friend ReadOnly Property red_flag() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("red-flag", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property thumbnail() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("thumbnail", resourceCulture)
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
